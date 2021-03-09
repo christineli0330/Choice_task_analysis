d681d990b81a6762ef213810baa4153b618e4ec8
Christine <xil485@ucsd.edu>
2021-03-09 13:17:35 -0800

    backup

---
+++ logistic_update_2_24.R
@@ -0,0 +1,56 @@
+## logistic plot for individual in delta value
+choice.beta = data.frame(ID=double(),
+                         beta=double(),
+                         p=double(),
+                         stringsAsFactors=FALSE)[1:length(id.filtered),]
+
+for (i in 1:length(id.filtered)) {
+  d =  choice %>% filter(ID == id.filtered[i], is.na(choseright)==FALSE)
+  logistic = glm(choseright ~ 1 + delta.value, data = d, family = "binomial")
+  beta = summary(logistic)[['coefficients']][2,1]
+  p = summary(logistic)[['coefficients']][2,4]
+  choice.beta$ID[i] = id.filtered[i]
+  choice.beta$beta[i] = beta
+  choice.beta$p[i] = p
+}
+
+filtered.b = choice.beta %>% filter(p<0.05, beta>0)
+
+choice = filter(choice, (ID %in% filtered.b$ID))
+
+choice %>% group_by(ID) %>% ggplot(aes(x = delta.value, y = choseright, group = ID)) + 
+  geom_point() + 
+  stat_smooth(method="glm", method.args=list(family="binomial"), se=FALSE)+
+  facet_wrap(~ID)
+
+
+## Plot trends of their rating task
+
+rid =  rating %>% filter(ID == choice.rtid$ID[1])
+plot(rid$trial_index, rid$response, '-s')
+
+rating = rating %>% mutate(response = as.numeric(response))
+rating %>% group_by(ID) %>% ggplot(aes(x = trial_index, y = response, group = ID)) + 
+  geom_point(color = "steelblue", size = 0.7) + geom_line(color = "steelblue3", alpha = 0.5) + 
+  facet_wrap(~ID)
+
+##test plot for old subjects
+rat.old = read.csv("food-choice-batch-2-12.csv") %>% filter(ttype == 'rating_task')
+rat.old$ID = cumsum(!duplicated(rat.old['run_id']))
+rat.old %>% group_by(ID) %>% ggplot(aes(x = trial_index, y = response, group = ID)) + 
+  geom_point(color = "steelblue", size = 0.7) + geom_line(color = "steelblue3", alpha = 0.5) + 
+  facet_wrap(~ID)
+
+
+rating %>%  group_by(ID) %>% dplyr::summarise(rt = mean(as.numeric(rt)))
+
+
+# try z-scored ratings
+rating = rating %>% group_by(ID) %>% mutate(z = scale(response))
+choice = choice %>% group_by(ID) %>% dplyr::mutate(z.delta.value = scale(delta.value))
+                    
+
+choice %>% group_by(ID) %>% ggplot(aes(x = z.delta.value, y = choseright, group = ID)) + 
+  geom_point() + 
+  stat_smooth(method="glm", method.args=list(family="binomial"), se=FALSE)+
+  facet_wrap(~ID)
