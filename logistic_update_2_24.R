942b0ccee9fbabd2842f158dea44de8d478675b6
Christine <xil485@ucsd.edu>
2021-03-29 12:15:18 -0700

    follow up

--- logistic_update_2_24.R
+++ logistic_update_2_24.R
@@ -14,9 +14,10 @@ for (i in 1:length(id.filtered)) {
   choice.beta$p[i] = p
 }
 
-filtered.b = choice.beta %>% filter(p<0.05, beta>0)
+filtered.b = choice.beta %>% filter(beta>0, p<0.05)
+#filtered.b = choice.beta %>% filter(p<0.05, beta>0)
 
-choice = filter(choice, (ID %in% filtered.b$ID))
+choice = filter(choice, (ID %in% filtered.b$ID | ID %in% c(10, 56)))
 
 choice %>% group_by(ID) %>% ggplot(aes(x = delta.value, y = choseright, group = ID)) + 
   geom_point() + 
@@ -47,10 +48,29 @@ rating %>%  group_by(ID) %>% dplyr::summarise(rt = mean(as.numeric(rt)))
 
 # try z-scored ratings
 rating = rating %>% group_by(ID) %>% mutate(z = scale(response))
-choice = choice %>% group_by(ID) %>% dplyr::mutate(z.delta.value = scale(delta.value))
+choice = choice %>% dplyr::group_by(ID) %>% dplyr::mutate(z.delta.value = scale(delta.value))
                     
 
 choice %>% group_by(ID) %>% ggplot(aes(x = z.delta.value, y = choseright, group = ID)) + 
   geom_point() + 
   stat_smooth(method="glm", method.args=list(family="binomial"), se=FALSE)+
   facet_wrap(~ID)
+
+
+
+
+
+## test the distribution of positions for more valuable items
+position = choice %>% dplyr::group_by(ID) %>% dplyr::mutate(posi = case_when(delta.value>0 ~ 1, delta.value<0 ~ 0, delta.value ==0 ~ 2))
+x = position %>% dplyr::group_by(ID) %>% filter(posi != 2) %>% dplyr::summarize(p = mean(posi))
+
+position %>% dplyr::group_by(ID) %>% dplyr::filter(posi == 2) %>% dplyr::summarize(p = mean(choseright))
+
+
+### multilevel model mixed-effects
+m1 = glmer(choseright~1+delta.mem+delta.value+(1+delta.mem+delta.value|ID), 
+           data = choice, family = "binomial")
+summary(m1)
+
+m2 = glmer(choseright~1+delta.mem+(1+delta.mem|ID), 
+           data = choice, family = "binomial")
